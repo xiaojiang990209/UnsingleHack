412b00c9d9467f8219457787dc69a9278e0bc589
h4xiang <h4xiang@uwaterloo.ca>
2018-03-17 19:24:48 -0400

    merged sign in activity

--- app/src/main/java/com/z224jian/singlehack/Sign_InActivity.java
+++ app/src/main/java/com/z224jian/singlehack/Sign_InActivity.java
@@ -1,14 +1,23 @@
 package com.z224jian.singlehack;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
@@ -21,72 +30,124 @@ import org.json.JSONObject;
 
 public class Sign_InActivity extends BaseActivity {
 
-    LoginButton loginButton;
-    CallbackManager callbackManager;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    private TextView info;
+    private AccessTokenTracker accessTokenTracker;
+    private ProfileTracker profileTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.sign_in);
 
-        // Create the facebook login button
-        loginButton = (LoginButton) findViewById(R.id.login_button);
-        loginButton.setReadPermissions("email", "public_profile");
+
+        info = (TextView)findViewById(R.id.info);
         callbackManager = CallbackManager.Factory.create();
 
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldToken, AccessToken currentToken) {
+
+            }
+        };
+
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile newProfile) {
+                nextActivity(newProfile);
+            }
+        };
+        accessTokenTracker.startTracking();
+        profileTracker.startTracking();
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_friends", "public_profile");
+
+        FacebookCallback<LoginResult> callback = new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
+                Profile profile = Profile.getCurrentProfile();
+                nextActivity(profile);
+                Toast.makeText(getApplicationContext(), "Loggin in...", Toast.LENGTH_SHORT).show();
+
+                info.setText("Login succeed.");
                 String userid = loginResult.getAccessToken().getUserId();
-                GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
-                    @Override
-                    public void onCompleted(JSONObject object, GraphResponse response) {
-                        displayUserInfo(object);
-                    }
-                });
-                Bundle parameters = new Bundle();
-                parameters.putString("fields", "first_name, last_name, email, id");
-                graphRequest.setParameters(parameters);
-                graphRequest.executeAsync();
+//                GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+//                    @Override
+//                    public void onCompleted(JSONObject object, GraphResponse response) {
+//                        displayUserInfo(object);
+//                    }
+//                });
+//                Bundle parameters = new Bundle();
+//                parameters.putString("fields", "first_name, last_name, id");
+//                graphRequest.setParameters(parameters);
+//                graphRequest.executeAsync();
             }
 
             @Override
             public void onCancel() {
+                info.setText("Login attempt canceled.");
 
             }
 
             @Override
             public void onError(FacebookException error) {
-
+                info.setText("Login attempt failed.");
             }
-        });
-    };
-
-    public void displayUserInfo(JSONObject object)
-    {
-        String first_name, last_name, email, id;
-        first_name = "";
-        last_name = "";
-        email = "";
-        id = "";
-        try {
-            first_name = object.getString("first_name");
-            last_name = object.getString("last_name");
-            email = object.getString("email");
-            id = object.getString("id");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        TextView tv_name, tv_email, tv_id;
-        tv_name = (TextView)findViewById(R.id.TV_name);
-        tv_email = (TextView)findViewById(R.id.TV_email);
-        tv_id = (TextView)findViewById(R.id.TV_id);
-
-        tv_name.setText(first_name + " " + last_name);
-        tv_email.setText("Email:" + email);
-        tv_id.setText("ID:"+id);
-
+        };
+        loginButton.registerCallback(callbackManager, callback);
     }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Profile profile = Profile.getCurrentProfile();
+        nextActivity(profile);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    protected void onStop() {
+        super.onStop();
+        accessTokenTracker.stopTracking();
+        profileTracker.stopTracking();
+    }
+
+    private void nextActivity(Profile profile) {
+        if(profile != null) {
+            Intent main = new Intent(Sign_InActivity.this, ProfileActivity.class);
+            main.putExtra("first_name", profile.getFirstName());
+            main.putExtra("last_name", profile.getLastName());
+            main.putExtra("imageUrl", profile.getProfilePictureUri(200,200). toString());
+            startActivity(main);
+        }
+    }
+
+//    public void displayUserInfo(JSONObject object)
+//    {
+//        String first_name, last_name, id;
+//        first_name = "";
+//        last_name = "";
+//        id = "";
+//        try {
+//            first_name = object.getString("first_name");
+//            last_name = object.getString("last_name");
+//            id = object.getString("id");
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//        TextView tv_name, tv_id;
+//        tv_name = (TextView)findViewById(R.id.TV_name);
+//        tv_id = (TextView)findViewById(R.id.TV_id);
+//
+//        tv_name.setText(first_name + " " + last_name);
+//        tv_id.setText("ID:"+id);
+//
+//    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
