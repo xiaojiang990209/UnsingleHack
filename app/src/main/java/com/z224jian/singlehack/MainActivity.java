8ade3635199f6e15fcad64adf62bbfe89ad82250
Zijie Jiang <z224jian@uwaterloo.ca>
2018-03-17 11:11:29 -0400

    commit

--- app/src/main/java/com/z224jian/singlehack/MainActivity.java
+++ app/src/main/java/com/z224jian/singlehack/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.telecom.Call;
 import android.view.View;
 import android.widget.TextView;
 
