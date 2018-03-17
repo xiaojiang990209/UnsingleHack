da3ee5e24012eb23ce9eb818657fe906e4823480
Chiheng Li <lichiheng1998@gmail.com>
2018-03-17 17:13:31 -0400

    pid

--- app/src/main/java/com/z224jian/singlehack/Matcher.java
+++ app/src/main/java/com/z224jian/singlehack/Matcher.java
@@ -2,6 +2,7 @@ package com.z224jian.singlehack;
 
 import android.util.Log;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -31,33 +32,46 @@ public class Matcher {
         ArrayList<Query> queryList = new ArrayList<>();
         // Operate every checked filter
         if (withLocation) {
-            queryList.add(mDatabase.orderByChild("Location").equalTo(postInfo.getLocation()));
+            queryList.add(mDatabase.child("Location").orderByChild(postInfo.getLocation()));
         }
         if (withCourseCode) {
-            queryList.add(mDatabase.orderByChild("Course").equalTo(postInfo.getCourseCode()));
+            queryList.add(mDatabase.child("Course").orderByChild(postInfo.getCourseCode()));
         }
         if (withGender) {
-            queryList.add(mDatabase.orderByChild("Gender").equalTo(postInfo.getGender()));
+            queryList.add(mDatabase.child("Gender").orderByChild(postInfo.getGender()));
         }
         // Add ValueEventListener to query
         for (Query query: queryList){
-            query.addValueEventListener(new ValueEventListener() {
+            query.addEventListener(new ChildEventListener() {
                 @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                     for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                         String pid = snapshot.getKey();
-                        Log.i("key", pid);
                         if (matchedPost.containsKey(pid)) {
                             matchedPost.put(pid,matchedPost.get(pid) + 1);
                         } else {
-                            matchedPost.put (pid, 0);
+                            matchedPost.put(pid, 0);
                         }
                     }
+                }
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
                 }
 
                 @Override
                 public void onCancelled(DatabaseError databaseError) {
-                    // No implementation for now
+
                 }
             });
         }
