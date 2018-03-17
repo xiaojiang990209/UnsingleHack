5acd023b3be51ce6064c9981a84f61ef0792beed
h4xiang <h4xiang@uwaterloo.ca>
2018-03-17 12:26:50 -0400

    update

--- app/src/main/java/com/z224jian/singlehack/Matcher.java
+++ app/src/main/java/com/z224jian/singlehack/Matcher.java
@@ -1,10 +1,17 @@
 package com.z224jian.singlehack;
 
+import com.z224jian.singlehack.Model.Post;
+
 /**
  * Created by orion on 2018-03-17.
  */
 
-public class Matcher
-{
+public class Matcher {
+    Post postInfo;
+
+    Matcher (Post new_post) {
+        this.postInfo = new_post;
+    }
+    
 
 }
